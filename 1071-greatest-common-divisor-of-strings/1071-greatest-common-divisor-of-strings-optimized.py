b645240afa90c612dd2f0058567fe8d2c85ba03b
Vishwajeet Ohal <54744950+vishwa702@users.noreply.github.com>
2025-03-03 22:36:50 -0700

    Time: 0 ms (100%), Space: 17.7 MB (92.44%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings-optimized.py
@@ -0,0 +1,16 @@
+from math import gcd
+
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1:  # Quick check if a common divisor string exists
+            return ""
+
+        gcd_length = gcd(len(str1), len(str2))
+        return str1[:gcd_length]
+
+
+        
+
+
+        
+
